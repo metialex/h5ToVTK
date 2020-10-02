27ffa79359ba683e51c54b37a90e87d6c1ff19d9
metelkin <metialex@gmail.com>
2020-10-02 10:27:12 +0200

    new option - dat

--- h5ToVTK.py
+++ h5ToVTK.py
@@ -153,14 +153,84 @@ def findFiles(path, keyword, index_min, index_max):
     files_res.sort()
     return files_res
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('-i', '--index', type=str,required=False, help='index range of data to convert (e.g. -i 2:5)')
-group = parser.add_mutually_exclusive_group()
-parser.add_argument('-el', '--eulerLagrangian', action='store_true', help='Convering Eulerian and Lagrangian data')
-parser.add_argument('-e', '--euler', action='store_true', help='Convering only Eulerian data')
-parser.add_argument('-l', '--lagrangian', action='store_true', help='Convering only Lagrangian data')
-args = parser.parse_args()
+def lagrangian_extract_quantity(output_path, quantity_list, files):
+    #--------------------Determine dimensions of each quantity-------------------------#
+    dimension_list = []
+    tmp = files[0].split('Particle_')[1]
+    with h5py.File(files[0], 'r') as hdf:
+        part_word = 'undefined'
+        try:
+            list(hdf.get('mobile').items())
+            part_word = 'mobile'
+        except:
+            try:
+                list(hdf.get('fixed').items())
+                part_word = 'fixed'
+            except:
+                print('Lagrangian Data is weird...')   
+        for quantity in quantity_list:
+            tmp1 = np.array(hdf.get(part_word).get(quantity))
+            dimension_list.append(len(tmp1[0]))
+    
+        #--------------------Fill out the data array-------------------------#
+    res = []#i - timestep, j- index of quantity, k -particle index
+    time = []
+    for input_file in files:
+        tmp = input_file.split('Particle_')[1]
+        print('Processing - ' + tmp + ' file')
+        file = output_path + '/Particle_' + tmp.split('.h5')[0] + '.vtk'
+        tmp2 = []
+        with h5py.File(input_file, 'r') as hdf:
+            time.append(hdf.get('time')[0])
+            print(time)
+            part_word = 'undefined'
+            try:
+                list(hdf.get('mobile').items())
+                part_word = 'mobile'
+            except:
+                try:
+                    list(hdf.get('fixed').items())
+                    part_word = 'fixed'
+                except:
+                    print('Lagrangian Data is weird...')
+            
+            #print(list(hdf.get('mobile').items()))
+            tmp1 = []    
+            for quantity in quantity_list:
+                tmp = np.array(hdf.get(part_word).get(quantity))
+                for i in range(len(tmp[0])):
+                    for j in range(len(tmp)):
+                        tmp1.append(tmp[j][i])
+                    tmp2.append(tmp1)
+                    tmp1 = [] 
+        res.append(tmp2)
 
+    #--------------------Write the array into the .dat file-------------------------#
+    for q in range(len(quantity_list)):
+        for qq in range(dimension_list[q]):
+            file = quantity_list[q] + str(qq) + '.dat'
+            f = open(file,'w')
+            for i in range(len(res)):
+                f.write(str(time[i]) + ',')
+                for j in res[i][qq]:
+                    f.write(str(j) + ',')
+                f.write('\n')
+    
+
+
+parser = argparse.ArgumentParser()
+
+
+parser.add_argument('-i', '--index', type=str,required=False, help='index range of data to convert (e.g. -i 2:5)')
+parser.add_argument('-d', '--dat', type=str,required=False, help='Converting .h5 file into .dat file.\
+     Avaliable variables - F_IBM, F_coll, F_rigid, Fc, Int_Omega_old,\
+          Int_U_old, Omega, R, Rotn, T_IBM, T_coll, T_rigid, Tc, U, X, collision_data')
+#group = parser.add_mutually_exclusive_group()
+parser.add_argument('-el', '--eulerLagrangian', action='store_true', help='Converting Eulerian and Lagrangian data')
+parser.add_argument('-e', '--euler', action='store_true', help='Converting only Eulerian data')
+parser.add_argument('-l', '--lagrangian', action='store_true', help='Converting only Lagrangian data')
+args = parser.parse_args()
+    
 #Create output directory
 directory = 'vtk_out/'
 try:
@@ -178,8 +248,14 @@ output_path = 'vtk_out/'
 index_min = 0
 index_max = 1e+10
 
-
-if args.eulerLagrangian:
+if args.dat:
+    quantity_list = []
+    for i in (args.dat.split(',')):
+        quantity_list.append(i)
+    files = findFiles(input_path,lagrang_keyword, index_min, index_max)
+    lagrangian_extract_quantity(directory, quantity_list, files)
+    
+elif args.eulerLagrangian:
     if args.index:
         index_min = int(args.index.split(':')[0])
         index_max = int(args.index.split(':')[1])
