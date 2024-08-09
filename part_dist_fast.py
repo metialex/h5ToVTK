d596556a5c6151443b96f71401a845fb0da32567
metialex <metialex@gmail.com>
2024-08-09 14:48:25 +0200

    updated part_dist_fast.py

--- part_dist_fast.py
+++ part_dist_fast.py
@@ -6,8 +6,14 @@ from tqdm import tqdm
 from subdomain_functions import set_subdomain,set_part_subdomain,get_adjacent_indices,add_p_to_subdomains
 import argparse
 
-def rand_part(Lx,Ly,Lz):
-    return np.random.rand(1,3)*np.array([Lx,Ly,Lz])
+def rand_part(Lx,Ly,Lz,per_bc,rad):
+    while True:
+        part = np.random.rand(1,3)*np.array([Lx,Ly,Lz])
+        if per_bc[0] == 0 and (part[0][0] < rad or part[0][0] > Lx-rad): continue
+        if per_bc[1] == 0 and (part[0][1] < rad or part[0][1] > Ly-rad): continue
+        if per_bc[2] == 0 and (part[0][2] < rad or part[0][2] > Lz-rad): continue
+        break
+    return part
 
 def calc_dist_3periodic(part1, part2, Lx, Ly, Lz):
     offsets = np.array([
@@ -39,6 +45,9 @@ parser.add_argument('-l','--domain_length', type=str,required=True,
                     help='Size of the domain in the format of Lx:Ly:Lz')
 parser.add_argument('-s','--subdomains_number', type=str,required=True,
                     help='Number of subdomains in the format of nx:ny:nz')
+parser.add_argument('-per_bc', '--periodic_bc', type=str, required=True,
+                    help='Periodic BC over axis, set in "0:1:1" format, where 1 correspond to periodic BC and\
+                    0 to wall BC')
 parser.add_argument('-o', '--out_folder', type=str, required=False,
                     help='Outpur folder for .pickle and .inp files')
 parser.add_argument('-v', '--visualize', type=bool, required=False,
@@ -60,17 +69,22 @@ phi = args.phi
 rad = args.radius
 delta = args.delta
 
+#Periodic BC
+per_bc = [int(args.periodic_bc.split(":")[0]),
+          int(args.periodic_bc.split(":")[1]),
+          int(args.periodic_bc.split(":")[2])]
+
 if args.out_folder:
-    out_folder = args.out_folder
-    if not os.path.exists(out_folder):
-        os.makedirs(out_folder)
+    out_folder = args.out_folder + "/"
+    if not os.path.exists(args.out_folder):
+        os.makedirs(args.out_folder)
         print("Output folder created")
     else: print("Output folder exists")
     
 else: out_folder = ""
 out_name_ptrn = f"p_mobile_phi_{str(phi)}_Lx_{str(Lx)}_Ly_{str(Ly)}_Lz_{str(Lz)}_r_{str(rad)}_delta_{str(delta)}"
-out_inp_file =    out_folder + "/" + out_name_ptrn + ".inp"
-out_pickle_file = out_folder + "/" + out_name_ptrn + ".pickle" 
+out_inp_file =    out_folder + out_name_ptrn + ".inp"
+out_pickle_file = out_folder + out_name_ptrn + ".pickle" 
 
 #Calculate total number of particles
 V_domain = Lx*Ly*Lz
@@ -81,7 +95,7 @@ part_list = []
 #Add particles to the list
 for i in tqdm(range(n_part),desc="Inserting particles"):
     while True:
-        part = rand_part(Lx,Ly,Lz).flatten()
+        part = rand_part(Lx,Ly,Lz,per_bc,rad).flatten()
         break_flag = True
 
         if len(part_list) > 0:
